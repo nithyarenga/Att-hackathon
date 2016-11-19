0da801ddadc0423bf9d72aaec9a3cbdc5ecc2fa3
Nithya Renganathan <nits.renga@gmail.com>
2016-11-19 08:24:06 +0000

    updated m2x

--- nithya/server.py
+++ nithya/server.py
@@ -4,9 +4,14 @@ from bottle import post, get, put, delete
 from pymongo import MongoClient
 from json import dumps
 from bson import json_util
+from m2x.client import M2XClient
+from datetime import datetime
+from requests.exceptions import HTTPError
 
 client = MongoClient()
 db = client.lighthouse
+
+client = M2XClient(key="dbbbb260442d2411fb43e2f1917a8d88")
 
 @get('/hello')
 def hello():
@@ -14,8 +19,21 @@ def hello():
 
 @route('/m2x_trigger')
 def m2x_trigger():
-    return "hello"
+    # Find the correct device if it exists, if not create it.
+    try:
+        device = [d for d in client.devices(q="stockreport-bluemix") if d.name == "stockreport-bluemix"][0]
+    except IndexError:
+        device = client.create_device(name="stockreport-bluemix",
+                                      description="Stockreport Example Device",
+                                      visibility="private")
 
+    # Get the stream if it exists, if not create the stream.
+    stream = device.stream("counter")
+    postime = datetime.now()
+    stream.add_value(db.put_counter.count(), postime)
+    #import ystockquote
+    #stock_price = ystockquote.get_price("T").encode('utf-8')
+    #stream.add_value(stock_price, postime)
 @get('/alerts')
 #color
 #list of strings
@@ -33,9 +51,15 @@ def returnarray():
 @get('/shelter')
 #name, address
 def returnarray():
-    rv = [{ "name": "welcome-home", "address": "111 legendary drive"}, { "name": "dallas life foundation", "address": "1234 life street"}]
-    response.content_type = 'application/json'
-    return dumps(rv)
+    if db.shelter_dir.count() == 0:
+        rv = [{ "name": "welcome-home", "address": "111 legendary drive"}, { "name": "dallas life foundation", "address": "1234 life street"}]
+        response.content_type = 'application/json'
+        return dumps(rv)
+    else:
+        # How to sort the database? based on location?
+        query = db.alerts.find()
+        for doc in query:
+            return json_util.dumps(doc)
 
 
 @get('/emergency')
